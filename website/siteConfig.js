14b3fb8a3e79bb581845fb8bea0c589ee0650af6
iamkun <kunhello@outlook.com>
2020-01-27 17:01:19 +0800

    update doc

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -99,7 +99,8 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/iamkun/dayjs'
+  repoUrl: 'https://github.com/iamkun/dayjs',
+  cname: 'dayjs.github.io'
 }
 
 module.exports = siteConfig
