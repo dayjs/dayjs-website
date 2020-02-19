5661ff0b7186b9f7cb919fa63fc7b25b5f9ba6cd
iamkun <kunhello@outlook.com>
2020-02-19 14:42:40 +0800

    update

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -41,7 +41,6 @@ class Footer extends React.Component {
               className="github-button"
               href={this.props.config.repoUrl}
               target="_blank"
-              data-icon="octicon-star"
               data-count-href="/iamkun/dayjs/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
