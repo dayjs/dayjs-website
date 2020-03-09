e9381bfc4b66ce03be084a7c24d9ec8dbacd4621
iamkun <kunhello@outlook.com>
2020-03-09 17:21:42 +0800

    update doc

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,7 +37,7 @@ const siteConfig = {
     { languages: true },
   ],
 
-  translationRecruitingLink: 'https://crowdin.com/project/dayjs',
+  translationRecruitingLink: 'https://github.com/dayjs/dayjs-website/issues/5',
 
   // If you have users set above, you add it here:
   // users,
