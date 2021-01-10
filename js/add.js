308346396cac5ca4f33fd7cef91c04cec96d66c9
iamkun <kunhello@outlook.com>
2021-01-10 14:57:37 +0000

    Deploy website
    Deploy website version based on 42d282c25151b6d26040d44a9b7638a8866f33bd

--- js/add.js
+++ js/add.js
@@ -1,12 +1,12 @@
-var isCN = location.href.indexOf('zh-CN') > -1
-if (isCN) {
-  const addArea = document.querySelector('nav.onPageNav')
-  if (addArea) {
-    window.onAddClick = function () {
-      ga && ga('send', 'event', 'JUMPADD', 'go');
+(function () {
+  const userLanguage = ((navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage)) || '').toLowerCase()
+  if (['zh', 'zh-cn'].indexOf(userLanguage) > -1) {
+    const addArea = document.querySelector('nav.onPageNav')
+    if (addArea) {
+      window.onAddClick = function () {
+        ga && ga('send', 'event', 'JUMPADD', 'go');
+      }
+      addArea.innerHTML = `<a onclick=\"onAddClick()\" href=\"https://gitee.com/iamkun/dayjs\" target=\"_blank\"><img src=\"https://s3.ax1x.com/2021/01/10/sler7t.png\"/><\/a>`
     }
-    addArea.innerHTML = `<a onclick=\"onAddClick()\" href=\"https://gitee.com/iamkun/dayjs\" target=\"_blank\"><img src=\"https://s3.ax1x.com/2021/01/10/sler7t.png\"/><\/a>`
   }
-}
-
-
+})()
--- js/sponsors.js
+++ js/sponsors.js
@@ -1,39 +1,42 @@
-var isCN = location.href.indexOf('zh-CN') > -1
-var isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent)
-var sideNav = document.querySelector('nav.toc')
-if (sideNav && !isMobile) {
-  var div = document.createElement("div");
-  var imgName = isCN ? 'duohui-cn' : 'duohui'
-  var title = isCN ? '赞助商' : 'Sponsors'
-  var strVar = "";
-  strVar += "<div class=\"sponsors\">\n";
-  strVar += "	<div class=\"title\">"+title+"<\/div>\n";
-  strVar += "	<div class=\"images\">\n";
-  strVar += "		<a href=\"https://www.duohui.cn?utm_source=dayjs\" title=\"多会 - 专业活动管理系统\" target=\"_blank\"><img class=\"en\" alt=\"多会\" src=\"/img/sponsors/"+imgName+".png\"/><\/a ><\/div>\n";
-  strVar += "<\/div>\n";
-  div.innerHTML = strVar
-  sideNav.prepend(div);
-  document.head.insertAdjacentHTML("beforeend", 
-  `<style>
-  .toc .sponsors {
-    padding-bottom: 5px;
+(function() {
+  var isCN = location.href.indexOf('zh-CN') > -1
+  var isMobile = /Android|iPhone|iPad/i.test(navigator.userAgent)
+  var sideNav = document.querySelector('nav.toc')
+  if (sideNav && !isMobile) {
+    var div = document.createElement("div");
+    var imgName = isCN ? 'duohui-cn' : 'duohui'
+    var title = isCN ? '赞助商' : 'Sponsors'
+    var strVar = "";
+    strVar += "<div class=\"sponsors\">\n";
+    strVar += "	<div class=\"title\">"+title+"<\/div>\n";
+    strVar += "	<div class=\"images\">\n";
+    strVar += "		<a href=\"https://www.duohui.cn?utm_source=dayjs\" title=\"多会 - 专业活动管理系统\" target=\"_blank\"><img class=\"en\" alt=\"多会\" src=\"/img/sponsors/"+imgName+".png\"/><\/a ><\/div>\n";
+    strVar += "<\/div>\n";
+    div.innerHTML = strVar
+    sideNav.prepend(div);
+    document.head.insertAdjacentHTML("beforeend", 
+    `<style>
+    .toc .sponsors {
+      padding-bottom: 5px;
+    }
+    .toc .sponsors .title{
+      padding-bottom: 10px;
+      color: #777;
+      font-weight: 300;
+      font-size: 14px;
+    }
+    .toc .sponsors .images {
+      min-height: 50px
+    }
+    .toc .sponsors .en {
+      height: 50px
+    }
+    </style>`)
   }
-  .toc .sponsors .title{
-    padding-bottom: 10px;
-    color: #777;
-    font-weight: 300;
-    font-size: 14px;
+  const enF = document.querySelector('.sponsor.sponsor-en')
+  const cnF = document.querySelector('.sponsor.sponsor-cn')
+  if (enF && cnF) {
+    (isCN ? cnF : enF).style.display = "block";
   }
-  .toc .sponsors .images {
-    min-height: 50px
-  }
-  .toc .sponsors .en {
-    height: 50px
-  }
-  </style>`)
-}
-const enF = document.querySelector('.sponsor.sponsor-en')
-const cnF = document.querySelector('.sponsor.sponsor-cn')
-if (enF && cnF) {
-  (isCN ? cnF : enF).style.display = "block";
-}
+
+})()
