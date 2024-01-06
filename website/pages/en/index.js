58e01f0f6ae651038e5bbc55a71ceeea32f3f74e
iamkun <kunhello@outlook.com>
2024-01-06 22:33:17 +0800

    chore: Update index.js

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -105,6 +105,9 @@ function Intro() {
           <a href="https://opencollective.com/sight-and-sound-ministries" target="_blank">
             <img src="https://user-images.githubusercontent.com/17680888/232316426-cb99b4cf-0ccb-4e73-a6ce-e16dba6aadf4.png" />
           </a>
+          <a href="https://opencollective.com/chudovo" target="_blank">
+            <img src="https://images.opencollective.com/chudovo/3c866f5/logo/256.png?height=256" />
+          </a>
           <a href="https://www.exoflare.com/open-source/?utm_source=dayjs&utm_campaign=open_source" target="_blank">
             <img src="https://user-images.githubusercontent.com/17680888/162761622-1407a849-0c41-4591-8aa9-f98114ec2092.png"/>
           </a>
