c80c79f2fd7e089530c684e61be308d5bad99bb1
iamkun <kunhello@outlook.com>
2026-08-31 18:55:14 +0800

    chore: update doc

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -108,9 +108,6 @@ function Intro() {
           <a href="https://www.netrouting.com" target="_blank" alt="Netrouting Dedicated Servers">
             <img width="70" src="https://netrouting.com/wp-content/uploads/2026/04/Logo-netrouting.png"/>
           </a>
-          <a href="https://anonstories.com" target="_blank">
-            <img alt="Instagram Story Viewer" width="70" src="https://avatars.githubusercontent.com/u/240702364?v=4" />
-          </a>
           <a href="https://automatio.ai" target="_blank" alt="Automatio AI">
             <img width="70" src="https://avatars.githubusercontent.com/u/1984909?v=4" />
           </a>
