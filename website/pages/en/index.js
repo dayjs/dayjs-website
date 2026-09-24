7be647ce39b67e2d261488ace310f4283d1f2d0d
iamkun <kunhello@outlook.com>
2026-09-24 17:15:58 +0800

    chore: update doc

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -129,11 +129,6 @@ function Intro() {
           Balancing coding projects and academics? Discover a professional <b><a style={{ color: '#000' }} href="https://edubirdie.com/" target="_blank">essay service</a></b> that can help streamline your workload.
         </div>
         <div style={{ marginTop: '24px', textAlign: 'left' }}>
-          <a style={{ color: '#000' }} href="https://essaypro.com/pay-for-research-paper" target="_blank">
-            pay EssayPro to write my research paper
-          </a>
-        </div>
-        <div style={{ marginTop: '24px', textAlign: 'left' }}>
           <a style={{ color: '#000' }} href="https://writepaper.com/" target="_blank">paper writing help at WritePaper</a>
         </div>
         <div style={{ marginTop: '24px', textAlign: 'left' }}>
